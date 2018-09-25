bf365432782fa98cc8445eadbbde95c224aedf8a
Naama T Gouti <egouti2@gmail.com>
2018-09-25 06:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays() {
-  return("Happy Holidays!");
+  return("Happy holidays!");
 }
 
 function happyHolidaysTo(name) {
-  return('Happy holidays, ${name}');
+  return("Happy holidays," + '${name}');
 }
 
 function happyHolidayTo(holiday, name) {
-  return('Happy ${holiday}, ${name}!');
+  return("Happy" + '${holiday}, ${name}!');
 }
